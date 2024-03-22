70ab86e89bb3abb7b547c1987278cbfa720a8dec
HBalev <139244140+HBalev@users.noreply.github.com>
2024-03-22 12:52:26 +0100

    Update ventilation_capacity_propagation.py

--- ventilation_capacity_propagation.py
+++ ventilation_capacity_propagation.py
@@ -40,21 +40,12 @@ def change_property_value_pset(element: ifcopenshell.entity_instance, psets_dict
 
 def process_table(table_path: str, empty_value: str, properties: dict[str, dict], table_data_column_nr: int) -> dict[
     str, dict]:  ###HARDCODED
-    print("process_table")
     tab = pd.read_excel(table_path, index_col=None, header=None)
-    # sav={}
-    # rav={}
-    # eav={}
-    # oav={}
-    # tpe={}
-    print(tab.shape)
     for k in range(1, tab.shape[0]):
         row = tab.iloc[k]
-        print(row)
         guid = row[0]
-        for i in range(1, table_data_column_nr):  # tab.shape[1]):
+        for i in range(1, table_data_column_nr):  
             if row[i] != empty_value and guid in properties.keys():
-                # print(guid, row[i])
                 str_val = row[i]
                 val = float(str_val.split(" m³/h")[0].replace(',', '.'))
                 if i == 1:
@@ -79,50 +70,20 @@ def propagation_procedure(graph: nx.classes.graph.Graph, source: list, target: l
     :param target - selection of objects that end the propagation
     :param properties - dictionary with keys-guids and value a dictionary of property values
     '''
-    print("propagation_procedure")
     mapping_sys_name = {"Zuluft": "SupplyAirVolume", "Abluft": "ReturnAirVolume", "Fortluft": "ExhaustAirVolume",
                         "Außenluft": "OutsideAirVolume"}
-    # propagate=lambda original,value : original+value
     for cc in nx.connected_components(graph):
         stations = ((src, trgt) for src in source for trgt in target if src in cc and trgt in cc)
         for src, trgt in stations:
             sh_path = nx.shortest_path(graph, source=src, target=trgt)
             sh_path.remove(src)
-            print(src, trgt, sh_path)
             for sys_name in mapping_sys_name.keys():
                 if sys_name in properties[src]["SystemType"]:
                     category_bin = mapping_sys_name[sys_name]
                     prop_value = properties[src][category_bin]
-                    # print(tkt)
                     for n in sh_path:
-                        # if n != src:
                         properties[n][category_bin] = properties[n][category_bin] + prop_value
-                        # print(sav[n])
-            # if "Zuluft" in properties[src]["SystemType"]:
-            #     prop_value = properties[src]["SupplyAirVolume"]
-            #     # print(tkt)
-            #     for n in sh_path:
-            #         #if n != src:
-            #             properties[n]["SupplyAirVolume"] = properties[n]["SupplyAirVolume"] + prop_value
-            #             # print(sav[n])
-            # elif "Abluft" in properties[src]["SystemType"]:
-            #     prop_value = properties[src]["ReturnAirVolume"]
-            #     # print(tkt)
-            #     for n in sh_path:
-            #         #if n != src:
-            #             properties[n]["ReturnAirVolume"] = properties[n]["ReturnAirVolume"] + prop_value
-            # elif "Fortluft" in properties[src]["SystemType"]:
-            #     prop_value = properties[src]["ExhaustAirVolume"]
-            #     # print(tkt)
-            #     for n in sh_path:
-            #         #if n != src:
-            #             properties[n]["ExhaustAirVolume"] = properties[n]["ExhaustAirVolume"] + prop_value
-            # elif "Außenluft" in properties[src]["SystemType"]:
-            #     prop_value = properties[src]["OutsideAirVolume"]
-            #     # print(tkt)
-            #     for n in sh_path:
-            #         #if n != src:
-            #             properties[n]["OutsideAirVolume"] = properties[n]["OutsideAirVolume"] + prop_value
+                        
     return properties
 
 
@@ -154,21 +115,23 @@ def cleanup_edges(graph: nx.classes.graph.Graph, model: ifcopenshell.file) -> nx
                 graph.remove_edge(n1, n2)
         elif extract_system(model, n1):  # case just the first node has a system
             if system_name_contradiction(model, n1,
-                                         n2):  # ("Zuluft" in extract_system(model, n1)[0].Name and "Abluft" in model.by_guid(n2).Name) or (Abluft" in extract_system(model, n1)[0].Name and "Zuluft" in model.by_guid(n2).Name):
+                                         n2):  
                 graph.remove_edge(n1, n2)
         elif extract_system(model, n2):  # case just the second node has a system
             if system_name_contradiction(model, n2,
-                                         n1):  # ("Zuluft" in extract_system(model, n2)[0].Name and "Abluft" in model.by_guid(n1).Name) or ("Abluft" in extract_system(model, n2)[0].Name and "Zuluft" in model.by_guid(n1).Name):
+                                         n1): 
                 graph.remove_edge(n1, n2)
         else:
             if name_name_contradiction(model, n1, n2) or name_name_contradiction(model, n2,
-                                                                                 n1):  # ("Zuluft" in model.by_guid(n1).Name and "Abluft" in model.by_guid(n2).Name) or ("Zuluft" in model.by_guid(n2).Name and "Abluft" in model.by_guid(n1).Name):
+                                                                                 n1):  
                 graph.remove_edge(n1, n2)
-    print(type(graph))
     return graph
 
 
 def capacity_propagation_ventilation(file_path, table_path):
+    '''
+    Implementation function based on the propagation of capacity value of air terminals given in table to all connected entities targeting the Rooftop Unit
+    '''
     model = ifcopenshell.open(file_path)
     selection = preprocess_capacity_propagation(model)
     sm = SelectionModel(model, selection)
